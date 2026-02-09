41e48f4fa0abfc5c344d3e0e9cfa2e402cf4f36d
Arun-Clarisco <arunprasad@clariscosolutions.com>
2026-02-09 17:45:42 +0530

    chnages on option

--- model/UserWallet.js
+++ model/UserWallet.js
@@ -1,35 +1,97 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
+const BALANCE_FACTOR = 10000000000;
+
+/* ---------- getter ---------- */
+function balanceGetter(value) {
+	if (typeof value !== "number") return value;
+	return value / BALANCE_FACTOR;
+}
+
+
 var walletSchema = new Schema({
-	"userId" : { type: mongoose.Schema.Types.ObjectId, index: true, ref: 'Users'},
-	"currencyId": { type: mongoose.Schema.Types.ObjectId, index: true, ref: 'CurrencySymbol'}, 
-	"amountTradeFee": {type: Number, default: 0},
+	"userId": { type: mongoose.Schema.Types.ObjectId, index: true, ref: 'Users' },
+	"currencyId": { type: mongoose.Schema.Types.ObjectId, index: true, ref: 'CurrencySymbol' },
+	"amountTradeFee": { type: Number, default: 0 },
 	// spot & main
-	"amount": {type: Number, default: 0},
-	"hold": {type: Number, default: 0},
+	"amount": { type: Number, default: 0, get: balanceGetter },
+	"hold": { type: Number, default: 0, get: balanceGetter },
 	// usd-m / perpetual
 	"perpetualAmount": { type: Number, default: 0 },
 	"perpetualHold": { type: Number, default: 0 },
 	// ico
-	"icoAmount": {type: Number, default: 0},
+	"icoAmount": { type: Number, default: 0 },
 	// staking
-	"stakingAmount": {type: Number, default: 0},
-	"stakingHold": {type: Number, default: 0},
+	"stakingAmount": { type: Number, default: 0 },
+	"stakingHold": { type: Number, default: 0 },
 	// p2p
-	"p2pAmount": {type: Number, default: 0},
-	"p2pHold": {type: Number, default: 0},
+	"p2pAmount": { type: Number, default: 0 },
+	"p2pHold": { type: Number, default: 0 },
 	// loan
-	"cryptoLoanAmount": {type: Number, default: 0},
-	"cryptoLoanHold": {type: Number, default: 0},
+	"cryptoLoanAmount": { type: Number, default: 0 },
+	"cryptoLoanHold": { type: Number, default: 0 },
 	//** Bear & Bull */
-	"gamePredictionAmount": {type: Number, default: 0},
-	"gamePredictionHold": {type: Number, default: 0},
+	"gamePredictionAmount": { type: Number, default: 0 },
+	"gamePredictionHold": { type: Number, default: 0 },
 	//** Simple-earning */
-	"simpleEarnAmount": {type: Number, default: 0},
-	"simpleEarnHold": {type: Number, default: 0},
+	"simpleEarnAmount": { type: Number, default: 0, get: balanceGetter },
+	"simpleEarnHold": { type: Number, default: 0, get: balanceGetter },
 	//**Options */
-	"optionsGameAmount": {type: Number, default: 0},
-	"optionsHold": {type: Number, default: 0},
+	"optionsGameAmount": { type: Number, default: 0 },
+	"optionsHold": { type: Number, default: 0 },
+
+	//**Alpha Bot */
+	"alphaBotAmount": { type: Number, default: 0 },
+	"alphaBotHold": { type: Number, default: 0 }
 });
-module.exports = mongoose.model('UserWallet', walletSchema, 'UserWallet')
+
+const amounts = ["amount", "hold",
+	"perpetualAmount", "perpetualHold",
+	"icoAmount", "stakingAmount",
+	"stakingHold", "p2pAmount", "p2pHold",
+	"cryptoLoanAmount", "cryptoLoanHold",
+	"gamePredictionAmount", "gamePredictionHold",
+	"simpleEarnAmount", "simpleEarnHold",
+	"optionsGameAmount", "optionsHold",
+	"alphaBotAmount", "alphaBotHold"
+]
+walletSchema.pre("save", function (next) {
+	if (this.isModified("amount")) {
+		this.amount = Math.round(this.amount * BALANCE_FACTOR);
+		console.log(this.amount);
+	}
+	next();
+});
+
+function scaleInc(next) {
+	const update = this.getUpdate();
+
+	if (update?.$set) {
+		for (const key in update.$set) {
+			if (amounts.includes(key)) {
+				update.$set[key] = Math.round(update.$set[key] * BALANCE_FACTOR);
+			}
+		}
+	}
+
+	if (update?.$inc) {
+		for (const key in update.$inc) {
+			update.$inc[key] = Math.round(update.$inc[key] * BALANCE_FACTOR);
+		}
+	}
+
+	this.setUpdate(update);
+	next();
+}
+
+walletSchema.pre("findOneAndUpdate", scaleInc);
+walletSchema.pre("updateOne", scaleInc);
+walletSchema.pre("updateMany", scaleInc);
+
+
+walletSchema.set("toJSON", { getters: true });
+walletSchema.set("toObject", { getters: true });
+
+
+module.exports = mongoose.model('UserWallet', walletSchema, 'UserWallet');
