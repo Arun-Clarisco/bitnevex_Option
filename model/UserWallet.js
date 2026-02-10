8ab7b91544d560604e247c61f1fa41fe813dea0b
Arun-Clarisco <arunprasad@clariscosolutions.com>
2026-02-10 09:59:30 +0530

    changes on userWallet

--- model/UserWallet.js
+++ model/UserWallet.js
@@ -18,32 +18,32 @@ var walletSchema = new Schema({
 	"amount": { type: Number, default: 0, get: balanceGetter },
 	"hold": { type: Number, default: 0, get: balanceGetter },
 	// usd-m / perpetual
-	"perpetualAmount": { type: Number, default: 0 },
-	"perpetualHold": { type: Number, default: 0 },
+	"perpetualAmount": { type: Number, default: 0, get: balanceGetter },
+	"perpetualHold": { type: Number, default: 0, get: balanceGetter },
 	// ico
-	"icoAmount": { type: Number, default: 0 },
+	"icoAmount": { type: Number, default: 0, get: balanceGetter },
 	// staking
-	"stakingAmount": { type: Number, default: 0 },
-	"stakingHold": { type: Number, default: 0 },
+	"stakingAmount": { type: Number, default: 0, get: balanceGetter },
+	"stakingHold": { type: Number, default: 0, get: balanceGetter },
 	// p2p
-	"p2pAmount": { type: Number, default: 0 },
-	"p2pHold": { type: Number, default: 0 },
+	"p2pAmount": { type: Number, default: 0, get: balanceGetter },
+	"p2pHold": { type: Number, default: 0, get: balanceGetter },
 	// loan
-	"cryptoLoanAmount": { type: Number, default: 0 },
-	"cryptoLoanHold": { type: Number, default: 0 },
+	"cryptoLoanAmount": { type: Number, default: 0, get: balanceGetter },
+	"cryptoLoanHold": { type: Number, default: 0, get: balanceGetter },
 	//** Bear & Bull */
-	"gamePredictionAmount": { type: Number, default: 0 },
-	"gamePredictionHold": { type: Number, default: 0 },
+	"gamePredictionAmount": { type: Number, default: 0, get: balanceGetter },
+	"gamePredictionHold": { type: Number, default: 0, get: balanceGetter },
 	//** Simple-earning */
 	"simpleEarnAmount": { type: Number, default: 0, get: balanceGetter },
 	"simpleEarnHold": { type: Number, default: 0, get: balanceGetter },
 	//**Options */
-	"optionsGameAmount": { type: Number, default: 0 },
-	"optionsHold": { type: Number, default: 0 },
+	"optionsGameAmount": { type: Number, default: 0, get: balanceGetter },
+	"optionsHold": { type: Number, default: 0, get: balanceGetter },
 
 	//**Alpha Bot */
-	"alphaBotAmount": { type: Number, default: 0 },
-	"alphaBotHold": { type: Number, default: 0 }
+	"alphaBotAmount": { type: Number, default: 0, get: balanceGetter },
+	"alphaBotHold": { type: Number, default: 0, get: balanceGetter }
 });
 
 const amounts = ["amount", "hold",
